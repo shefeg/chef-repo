d8f6742e2fddcd2521b22f2f1405b88a8cb060d8
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-03-13 14:32:05 +0200

    Refactor

--- cookbooks/wp-app/attributes/default.rb
+++ cookbooks/wp-app/attributes/default.rb
@@ -11,9 +11,9 @@ when 'debian'
   default['mysql']['package']     = 'mysql-server'
   default['mysql']['service']     = 'mysql'
   default['repositories'] = {
-    "apt_repository" => ['php'],
-    "uri"            => ['ppa:ondrej/php'],
-    "components"     => ['main']
+    'apt_repository' => ['php'],
+    'uri'            => ['ppa:ondrej/php'],
+    'components'     => ['main']
   }
 when 'rhel'
   default['package_list']         = ['mysql-community-client', 'php', 'php-common', 'php-mysqlnd', 'php-gd', 'php-xml',
@@ -25,18 +25,17 @@ when 'rhel'
   default['mysql']['package']     = 'mysql-community-server'
   default['mysql']['service']     = 'mysqld'
   default['repositories'] = {
-    "yum_repository" => ['epel', 
+    'yum_repository' => ['epel',
                          'mysql57-community',
                          'remi-safe',
                          'remi-php72'],
-    "description"    => ['Extra Packages for Enterprise Linux 7 - $basearch',
+    'description'    => ['Extra Packages for Enterprise Linux 7 - $basearch',
                          'MySQL 5.7 Community Server',
                          'Safe Remi\'s RPM repository for Enterprise Linux $releasever - $basearch',
                          'Remi\'s PHP 7.2RPM repository for Enterprise Linux 7 – $basearch'],
-    "baseurl"        => ["http://download.fedoraproject.org/pub/epel/#{node['platform_version'].to_i}/$basearch/",
+    'baseurl'        => ["http://download.fedoraproject.org/pub/epel/#{node['platform_version'].to_i}/$basearch/",
                          "http://repo.mysql.com/yum/mysql-5.7-community/el/#{node['platform_version'].to_i}/$basearch/",
                          "http://rpms.remirepo.net/enterprise/#{node['platform_version'].to_i}/safe/$basearch/",
                          "http://rpms.remirepo.net/enterprise/#{node['platform_version'].to_i}/php72/$basearch/"]
   }
 end
-
